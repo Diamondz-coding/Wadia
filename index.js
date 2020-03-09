08c9631101b4bdae91ee1c689d10f0b652487cfb
Diamondz-coding <61663747+Diamondz-coding@users.noreply.github.com>
2020-03-09 21:18:13 +0000

    UPDATE!
    Updated The Bot

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+const token = 'NjgzNjc1NTY3NzQ0NDgzNTgw.XmarTQ.TurUIci06Lju-ovxz9HTB88YxbY';
 
-const token = 'NjgzNjc1NTY3NzQ0NDgzNTgw.Xmak6A.1l14gUOZxJ5w0wEdvF7KDib5RAg';
+var PREFIX = '?';
+bot.on('ready', () =>{
+  console.log('This Bot Is Online!')
+bot.on('message', message=>{
+  let args = message.content.substring(PREFIX.length).split(' '); x
+  switch(args[0]){
 
-bot.login(process.env.token)
+  case 'clear':
+  if(!args[1]) return message.reply('Error 101; Please Specify Amount; Like This: "?clear 12" or "?clear 134" ')
+       message.channel.bulkDelete(args[1]);
+  break;
+
+  case "roll":
+  var result = Math.floor(Math.random() * 8);
+  if(!args[1]){
+     message.reply("What Do You Want Me To Roll I Dont Read Minds! :triumph:")
+  }
+  if(args[1] === "dice")
+  {
+    message.reply(result)
+  }
+  case "about":
+
+  if(!args[1])
+  {
+  message.reply("Who Do You Want To Know About?")
+  }
+  break;
+
+  case "hello":
+       message.reply("Hello My Friend!")
+  break;
+
+}    }) })
+
+  bot.login(process.env.token);
