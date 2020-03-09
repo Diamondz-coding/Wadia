c5093ae699c5bba6dcdc2994a13ac75a9ca31383
LiamWM4 <61989316+LiamWM4@users.noreply.github.com>
2020-03-09 20:59:31 +0000

    JUST COMMIT IT

--- index.js
+++ index.js
@@ -5,38 +5,36 @@ const token = 'NjgzNjc1NTY3NzQ0NDgzNTgw.XmarTQ.TurUIci06Lju-ovxz9HTB88YxbY';
 var PREFIX = '?';
 bot.on('ready', () =>{
   console.log('This Bot Is Online!')
-  bot.on('message', message=>{ // allows bot to read any messages made by people
-let args = message.content.substring(PREFIX.length).split(' '); // defies the length of the prefix
-switch(args[0]){
-    case "memes":
-     message.channel.send("My Favourite Memes!\n Number 1: Rick Roll \n Number 2: Friday \n Number 3: Doge \n Number 4: Crab Rave \n And Last But Not Least: Robbie Rotton!") * 3
-break;
+bot.on('message', message)=>{
+  let args = message.content.substring(PREFIX.length).split(' '); x
+  switch(args[0]){
 
-case 'clear':
-if(!args[1]) return message.reply('Error 101; Please Specify Amount; Like This: "?clear 12" or "?clear 134" ')
-     message.channel.bulkDelete(args[1]);
-break;
+  case 'clear':
+  if(!args[1]) return message.reply('Error 101; Please Specify Amount; Like This: "?clear 12" or "?clear 134" ')
+       message.channel.bulkDelete(args[1]);
+  break;
 
-case "roll":
-var result = Math.floor(Math.random() * 8);
-if(!args[1]){
-   message.reply("What Do You Want Me To Roll I Dont Read Minds! :triumph:")
-}
-if(args[1] === "dice")
-{
-  message.reply(result)
-}
-case "about":
+  case "roll":
+  var result = Math.floor(Math.random() * 8);
+  if(!args[1]){
+     message.reply("What Do You Want Me To Roll I Dont Read Minds! :triumph:")
+  }
+  if(args[1] === "dice")
+  {
+    message.reply(result)
+  }
+  case "about":
 
-if(!args[1])
-{
-message.reply("Who Do You Want To Know About?")
-}
-break;
+  if(!args[1])
+  {
+  message.reply("Who Do You Want To Know About?")
+  }
+  break;
 
-case "hello":
-     message.reply("Hello My Friend!")
-break;
-    }
-})
-bot.login(process.env.token);
+  case "hello":
+       message.reply("Hello My Friend!")
+  break;
+}    } }
+
+
+  bot.login(process.env.token);
