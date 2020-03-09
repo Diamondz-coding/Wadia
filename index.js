5b7b0238a7d66fab3673ab0c7ab697338134d41d
Diamondz-coding <61663747+Diamondz-coding@users.noreply.github.com>
2020-03-09 19:58:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,74 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+
+
+
+
+
+
 const token = 'NjgzNjc1NTY3NzQ0NDgzNTgw.XmZxSA.nV3IOqulLp_rn8uZdVnPc1jCtLQ';
 
 var PREFIX = '?';
 
+
 bot.on('ready', () =>{
- console.log('This Bot Is Online!')
+    console.log('This Bot Is Online!')
 })
 
 bot.on('message', message=>{ // allows bot to read any messages made by people
-let args = message.content.substring(PREFIX.length).split(' '); // defies the length of the prefix
-switch(args[0]){
+    
+    let args = message.content.substring(PREFIX.length).split(' '); // defies the length of the prefix
+    
+    switch(args[0]){
 
-// Start of 'Fun commands':
-case "memes":
- message.channel.send("My Favourite Memes!\n Number 1: Rick Roll \n Number 2: Friday \n Number 3: Doge \n Number 4: Crab Rave \n And Last But Not Least: Robbie Rotton!") * 3
-break;
+        // Start of 'Fun commands':
 
-case 'CLEAR':
-if(!args[1]) return message.reply('Error 101; Please Specify Amount; Like This: "?clear 12" or "?clear 134" ')
-  message.channel.bulkDelete(args[1]);
-break;
+        case "memes":
+                        
+                    
+                    
+                        message.channel.send("My Favourite Memes!\n Number 1: Rick Roll \n Number 2: Friday \n Number 3: Doge \n Number 4: Crab Rave \n And Last But Not Least: Robbie Rotton!") * 3
+                    break;
+                    
+                    case 'CLEAR':
+                    if(!args[1]) return message.reply('Error 101; Please Specify Amount; Like This: "?clear 12" or "?clear 134" ')
+                    message.channel.bulkDelete(args[1]);
+                    
+                    break;
+                    
+                
 
-case 'clear':
-if(!args[1]) return message.reply('Error 101; Please Specify Amount; Like This: "?clear 12" or "?clear 134" ')
- message.channel.bulkDelete(args[1]);
-break;
+                        case 'clear':
+                            if(!args[1]) return message.reply('Error 101; Please Specify Amount; Like This: "?clear 12" or "?clear 134" ')
+                            message.channel.bulkDelete(args[1]);
+                            
+                            break;
 
-case "roll":
-var result = Math.floor(Math.random() * 8);
-if(!args[1]){
- message.reply("What Do You Want Me To Roll I Dont Read Minds! :triumph:")
-}
-if(args[1] === "dice")
-{
- message.reply(result)
-}
+                            
+                            case "roll":
+                              
+                            var result = Math.floor(Math.random() * 8);
+
+
+                                if(!args[1]){
+                                    message.reply("What Do You Want Me To Roll I Dont Read Minds! :triumph:")
+                                
+                                }
+                                if(args[1] === "dice"){
+                                    message.reply(result)
+                                }
 case "about":
-if(!args[1])
-{
-message.reply("Who Do You Want To Know About?")
-}
+                                 if(!args[1]){
+                                     message.reply("Who Do You Want To Know About?")
+                                 }  
+                                 if(args[1] === "wadia"){
+                                     message.channel.send("Hi, I'm Wadia! I Was Created By People Called 'Diamondz' And 'Wave' If You Would Like To Look How I Was Made; Look at: 'https://github.com/Diamondz-coding/Wadia' ! ")
+                                 } 
 break;
-
-case "hello":
- message.reply("Hello My Friend!")
-break;
-
-}
+                                
+                                case "hello":
+                                message.reply("Hello My Friend!")
+                                break;
+                            }
 })
 bot.login(process.env.token);
