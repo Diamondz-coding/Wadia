ae185e26414102439062457dcc16b1789c74a50d
Diamondz-coding <61663747+Diamondz-coding@users.noreply.github.com>
2020-03-09 20:16:12 +0000

    DELETED EVERYTHING **RESTART**

--- index.js
+++ index.js
@@ -3,24 +3,5 @@ const bot = new Discord.Client();
 
 const token = 'NjgzNjc1NTY3NzQ0NDgzNTgw.Xmahdw.hIT3h76npAtvQSVrwGe997qKW_0';
 
-var PREFIX = '?';
 
-
-bot.on('ready', () =>{
-    console.log('This Bot Is Online!')
-})
-
-bot.on('message', message=>{
-    
-    let args = message.content.substring(PREFIX.length).split(' ');
-    
-    switch(args[0]){
-        case 'hello':
-            message.author.send("What Up Mah Nigga!")
-
-       
-
-        
-}
-})
 bot.login(process.env.token)
